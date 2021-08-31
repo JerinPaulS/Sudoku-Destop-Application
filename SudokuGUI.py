622d723adc41028073fcdb1ca255b698a5032ee7
JerinPaulS <jerinsprograms@gmail.com>
2021-08-31 21:51:30 +0530

    Layout changed from Tkinter to Pygame

--- SudokuGUI.py
+++ SudokuGUI.py
@@ -1,78 +1,230 @@
-import Tkinter as tk
+import pygame
+from SudokuSolve import solve, valid
+import time
+pygame.font.init()
 
-def grid_layout(win, grid_dim):
-    entries = []
 
-    for row in range(grid_dim):
-        for col in range(grid_dim):
-            if question[row][col] == ".":
-                entry = tk.Entry(win, width = 3, font=("Calibri 10"), justify = 'center')
+class Grid:
+    board = [
+        [7, 8, 0, 4, 0, 0, 1, 2, 0],
+        [6, 0, 0, 0, 7, 5, 0, 0, 9],
+        [0, 0, 0, 6, 0, 1, 0, 7, 8],
+        [0, 0, 7, 0, 4, 0, 2, 6, 0],
+        [0, 0, 1, 0, 5, 0, 9, 3, 0],
+        [9, 0, 4, 0, 6, 0, 0, 0, 5],
+        [0, 7, 0, 3, 0, 0, 0, 1, 2],
+        [1, 2, 0, 0, 0, 7, 4, 0, 0],
+        [0, 4, 9, 2, 0, 6, 0, 0, 7]
+    ]
+
+    def __init__(self, rows, cols, width, height):
+        self.rows = rows
+        self.cols = cols
+        self.cubes = [[Cube(self.board[i][j], i, j, width, height) for j in range(cols)] for i in range(rows)]
+        self.width = width
+        self.height = height
+        self.model = None
+        self.selected = None
+
+    def update_model(self):
+        self.model = [[self.cubes[i][j].value for j in range(self.cols)] for i in range(self.rows)]
+
+    def place(self, val):
+        row, col = self.selected
+        if self.cubes[row][col].value == 0:
+            self.cubes[row][col].set(val)
+            self.update_model()
+
+            if valid(self.model, val, (row,col)) and solve(self.model):
+                return True
             else:
-                val = tk.StringVar()
-                entry = tk.Entry(win, width = 3, font=("Calibri 10"), textvariable = val, state = tk.DISABLED, justify = 'center')
-                val.set(question[row][col])
-            pad_y = (0, 0)
-            pad_x = (0, 0)
-            if (row + 1) % 3 == 0 and (row + 1) < grid_dim:
-                pad_y = (0, 3)
-            if (col + 1) % 3 == 0 and (col + 1) < grid_dim:
-                pad_x = (0, 3)
-            entry.grid(row = row, column = col, ipadx = 15, ipady = 15, padx = pad_x, pady = pad_y)
-            entries.append(entry)
-    return entries
-
-def start_func():
-    return 0
-
-def clear_func():
-    return 0
-
-def solveSudoku():
-    board = question
-    if solve(0, 0, board):
-        return board
-
-def solve(row, col, board):
-    for i in range(9):
-        for j in range(9):
-            if board[i][j] == '.':
-                for c in range(1, 10):
-                    if isvalid(i, j, str(c), board):
-                        board[i][j]= str(c)
-                        if solve(row, col, board) == True:
-                            return True
-                        board[i][j] = '.'
+                self.cubes[row][col].set(0)
+                self.cubes[row][col].set_temp(0)
+                self.update_model()
                 return False
-    return True
 
-def isvalid(row, col, c, board):
-    for i in range(9):
-        if board[i][col] == c:
-            return False
-        if board[row][i] == c:
-            return False
-        if board[3 * (row // 3) + i // 3][3 * (col // 3) + i % 3] == c:
-            return False
-    return True
+    def sketch(self, val):
+        row, col = self.selected
+        self.cubes[row][col].set_temp(val)
 
-question = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+    def draw(self, win):
+        # Draw Grid Lines
+        gap = self.width / 9
+        for i in range(self.rows+1):
+            if i % 3 == 0 and i != 0:
+                thick = 4
+            else:
+                thick = 1
+            pygame.draw.line(win, (0,0,0), (0, i*gap), (self.width, i*gap), thick)
+            pygame.draw.line(win, (0, 0, 0), (i * gap, 0), (i * gap, self.height), thick)
 
-win = tk.Tk()
-win.minsize(550, 530)
-win.maxsize(550, 530)
-win.title('Sudoku')
-entries = grid_layout(win, 9)
+        # Draw Cubes
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.cubes[i][j].draw(win)
 
-button1 = tk.Button(win, width = 3, text = "Start", command = start_func)
-button1.grid(row = 15, column = 1)
+    def select(self, row, col):
+        # Reset all other
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.cubes[i][j].selected = False
 
-button2 = tk.Button(win, width = 3, text = "Clear", command = clear_func)
-button2.grid(row = 15,column = 3)
+        self.cubes[row][col].selected = True
+        self.selected = (row, col)
 
-button3 = tk.Button(win, width = 3, text = "Solve", command = solveSudoku)
-button3.grid(row = 15,column = 5)
+    def clear(self):
+        row, col = self.selected
+        if self.cubes[row][col].value == 0:
+            self.cubes[row][col].set_temp(0)
 
-timer = tk.Entry(win, width = 5)
-timer.grid(row = 15, column = 7)
+    def click(self, pos):
+        """
+        :param: pos
+        :return: (row, col)
+        """
+        if pos[0] < self.width and pos[1] < self.height:
+            gap = self.width / 9
+            x = pos[0] // gap
+            y = pos[1] // gap
+            return (int(y),int(x))
+        else:
+            return None
 
-win.mainloop()
+    def is_finished(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if self.cubes[i][j].value == 0:
+                    return False
+        return True
+
+
+class Cube:
+    rows = 9
+    cols = 9
+
+    def __init__(self, value, row, col, width ,height):
+        self.value = value
+        self.temp = 0
+        self.row = row
+        self.col = col
+        self.width = width
+        self.height = height
+        self.selected = False
+
+    def draw(self, win):
+        fnt = pygame.font.SysFont("comicsans", 40)
+
+        gap = self.width / 9
+        x = self.col * gap
+        y = self.row * gap
+
+        if self.temp != 0 and self.value == 0:
+            text = fnt.render(str(self.temp), 1, (128,128,128))
+            win.blit(text, (x+5, y+5))
+        elif not(self.value == 0):
+            text = fnt.render(str(self.value), 1, (0, 0, 0))
+            win.blit(text, (x + (gap/2 - text.get_width()/2), y + (gap/2 - text.get_height()/2)))
+
+        if self.selected:
+            pygame.draw.rect(win, (255,0,0), (x,y, gap ,gap), 3)
+
+    def set(self, val):
+        self.value = val
+
+    def set_temp(self, val):
+        self.temp = val
+
+
+def redraw_window(win, board, time, strikes):
+    win.fill((255,255,255))
+    # Draw time
+    fnt = pygame.font.SysFont("comicsans", 40)
+    text = fnt.render("Time: " + format_time(time), 1, (0,0,0))
+    win.blit(text, (540 - 160, 560))
+    # Draw Strikes
+    text = fnt.render("X " * strikes, 1, (255, 0, 0))
+    win.blit(text, (20, 560))
+    # Draw grid and board
+    board.draw(win)
+
+
+def format_time(secs):
+    sec = secs % 60
+    minute = secs // 60
+    hour = secs // (60 * 60)
+
+    hour = ("{0:2f}".format(hour))
+    minute = ("{0:2f}".format(minute))
+    sec = ("{0:2f}".format(sec))
+
+    mat = hour + ":" + minute + ":" + sec
+    return mat
+
+
+def main():
+    win = pygame.display.set_mode((540,600))
+    pygame.display.set_caption("Sudoku")
+    board = Grid(9, 9, 540, 540)
+    key = None
+    run = True
+    start = time.time()
+    strikes = 0
+    while run:
+
+        play_time = round(time.time() - start)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    key = 1
+                if event.key == pygame.K_2:
+                    key = 2
+                if event.key == pygame.K_3:
+                    key = 3
+                if event.key == pygame.K_4:
+                    key = 4
+                if event.key == pygame.K_5:
+                    key = 5
+                if event.key == pygame.K_6:
+                    key = 6
+                if event.key == pygame.K_7:
+                    key = 7
+                if event.key == pygame.K_8:
+                    key = 8
+                if event.key == pygame.K_9:
+                    key = 9
+                if event.key == pygame.K_DELETE:
+                    board.clear()
+                    key = None
+                if event.key == pygame.K_RETURN:
+                    i, j = board.selected
+                    if board.cubes[i][j].temp != 0:
+                        if board.place(board.cubes[i][j].temp):
+                            print("Success")
+                        else:
+                            print("Wrong")
+                            strikes += 1
+                        key = None
+
+                        if board.is_finished():
+                            print("Game over")
+                            run = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                clicked = board.click(pos)
+                if clicked:
+                    board.select(clicked[0], clicked[1])
+                    key = None
+
+        if board.selected and key != None:
+            board.sketch(key)
+
+        redraw_window(win, board, play_time, strikes)
+        pygame.display.update()
+
+
+main()
+pygame.quit()
